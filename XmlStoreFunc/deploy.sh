78b1e2cd43fbdb09e5e5dd84add75cfccd157b42
Peter <plasne@iname.com>
2017-06-05 19:17:22 -0400

    more fixes

--- XmlStoreFunc/deploy.sh
+++ XmlStoreFunc/deploy.sh
@@ -1,7 +1,7 @@
-sudo mkdir /usr/local/customize
-sudo curl "https://github.com/plasne/ADFHDIPIG-pipeline/blob/master/XmlStoreFunc/azure-log4j.jar" > /usr/local/customize/azure-log4j.jar
-sudo curl "https://github.com/plasne/ADFHDIPIG-pipeline/blob/master/XmlStoreFunc/azure-api-0.4.4.jar" > /usr/local/customize/azure-api-0.4.4.jar
-sudo curl "https://pelasnepigstore.blob.core.windows.net/pelasne-pig-2017-03-09t22-26-52-136z/user/plasne/azure-log4j.properties?st=2017-06-05T21%3A51%3A00Z&se=2017-06-30T21%3A51%3A00Z&sp=r&sv=2015-12-11&sr=b&sig=qc8jUd3SYD6pni6xyJC%2FXql8VK%2Fkxv%2FuJgqO5%2B%2BqIGc%3D" > /usr/local/customize/azure-log4j.properties
-sudo echo "export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/local/customize/*" >> /etc/hadoop/conf/hadoop-env.sh
-sudo export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/local/customize/*
-sudo echo "log4jconf=/usr/local/customize/azure-log4j.properties" >> /etc/pig/conf/pig.properties
+mkdir /usr/local/customize
+curl "https://github.com/plasne/ADFHDIPIG-pipeline/blob/master/XmlStoreFunc/azure-log4j.jar" > /usr/local/customize/azure-log4j.jar
+curl "https://github.com/plasne/ADFHDIPIG-pipeline/blob/master/XmlStoreFunc/azure-api-0.4.4.jar" > /usr/local/customize/azure-api-0.4.4.jar
+curl "https://pelasnepigstore.blob.core.windows.net/pelasne-pig-2017-03-09t22-26-52-136z/user/plasne/azure-log4j.properties?st=2017-06-05T21%3A51%3A00Z&se=2017-06-30T21%3A51%3A00Z&sp=r&sv=2015-12-11&sr=b&sig=qc8jUd3SYD6pni6xyJC%2FXql8VK%2Fkxv%2FuJgqO5%2B%2BqIGc%3D" > /usr/local/customize/azure-log4j.properties
+printf "\n\nexport HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/local/customize/*" >> /etc/hadoop/conf/hadoop-env.sh
+export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:/usr/local/customize/*
+printf "\n\nlog4jconf=/usr/local/customize/azure-log4j.properties" >> /etc/pig/conf/pig.properties
