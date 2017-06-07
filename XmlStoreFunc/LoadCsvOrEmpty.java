01b9079efa421c3a8713cfb7186b7b17fe90c453
Peter <plasne@iname.com>
2017-06-07 17:25:37 -0400

    read dir

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -2,6 +2,11 @@ package input;
 
 import java.io.IOException;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.pig.piggybank.storage.CSVLoader;
 
 public class LoadCsvOrEmpty extends CSVLoader {
@@ -11,7 +16,31 @@ public class LoadCsvOrEmpty extends CSVLoader {
 
   @Override
   public void setLocation(String location, Job job) throws IOException {
-    System.out.println("location: " + location);
+
+    // support local and hadoop
+    if (config.startsWith("./")) {
+
+      // read from the local file system
+      raw = new String(Files.readAllBytes(Paths.get(config)), StandardCharsets.UTF_8);
+
+    } else {
+
+      // see if there are files to process
+      Configuration conf = udfc.getJobConf();
+      FileSystem fs = FileSystem.get(conf);
+      RemoteIterator<LocatedFileStatus> i_fs = fs.listFiles(new Path(location), true);
+      while (i_fs.hasNext()) {
+        LocatedFileStatus status = i_fs.next();
+        if (status.isFile() && status.getBlockSize() > 0) {
+          System.out.println("process: " + status.getPath().getName());
+        } else {
+          System.out.println("ignore: " + status.getPath().getName());
+        }
+      }
+      fs.close();
+
+    }
+
     super.setLocation(location, job);
   }
 
--- XmlStoreFunc/load.pig
+++ XmlStoreFunc/load.pig
@@ -1,5 +1,5 @@
 REGISTER input.jar;
 REGISTER json-simple-1.1.jar;
 
-raw = LOAD '/user/plasne/CUSTOMER.201705041622.csv' USING input.LoadCsvOrEmpty();
+raw = LOAD '/user/plasne/input' USING input.LoadCsvOrEmpty();
 DUMP raw;
