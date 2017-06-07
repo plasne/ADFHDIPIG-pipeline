3b2116c31929111d929f04b9439eede6f4a51c2b
Peter <plasne@iname.com>
2017-06-07 15:40:49 -0400

    more

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -4,4 +4,14 @@ import org.apache.pig.piggybank.storage.CSVLoader;
 
 class LoadCsvOrEmpty extends CSVLoader {
 
+  public LoadCsvOrEmpty(String delim) {
+    super(delim);
+  }
+
+  @Override
+  public void setLocation(String location, Job job) throws IOException {
+    System.out.println("location: " + location);
+    super.setLocation(location, job);
+  }
+
 }
---
+++ XmlStoreFunc/load.pig
@@ -0,0 +1,4 @@
+REGISTER input.jar;
+REGISTER json-simple-1.1.jar;
+
+raw = LOAD '/user/plasne/CUSTOMER.201705041622.csv' USING input.LoadCsvOrEmpty(',');
