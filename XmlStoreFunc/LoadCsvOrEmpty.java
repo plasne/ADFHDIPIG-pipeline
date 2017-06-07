c412e3446fc52641105e7a8080dd7db64eab6bb3
Peter <plasne@iname.com>
2017-06-07 18:57:30 -0400

    push

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -17,46 +17,36 @@ public class LoadCsvOrEmpty extends CSVLoader {
 
   @Override
   public void setLocation(String location, Job job) throws IOException {
-    System.out.println("!!!!!!!!!!!!! start setLocation");
     UDFContext udfc = UDFContext.getUDFContext();
     if (!udfc.isFrontend()) { // only read on backend
       String folder = location.replace("file:", "");
-
-      System.out.println("!!!!!!!!!!!!! backend");
+      warn("BACKEND", PigWarning.UDF_WARNING_1);
 
       // support local and hadoop
       if (folder.startsWith("./")) {
-
-        System.out.println("!!!!!!!!!!!!! wrong");
 
         // read from the local file system
         //raw = new String(Files.readAllBytes(Paths.get(config)), StandardCharsets.UTF_8);
 
       } else {
 
-        System.out.println("!!!!!!!!!!!!! process");
-
         // see if there are files to process
         Configuration conf = udfc.getJobConf();
         FileSystem fs = FileSystem.get(conf);
         RemoteIterator<LocatedFileStatus> i_fs = fs.listFiles(new Path(folder), true);
-        System.out.println("!!!!!!!!!!!!! listed");
         while (i_fs.hasNext()) {
           LocatedFileStatus status = i_fs.next();
           if (status.isFile() && status.getBlockSize() > 0) {
-            System.out.println("process: " + status.getPath().getName());
+            warn("process: " + status.getPath().getName(), PigWarning.UDF_WARNING_2);
           } else {
-            System.out.println("ignore: " + status.getPath().getName());
+            warn("ignore: " + status.getPath().getName(), PigWarning.UDF_WARNING_3);
           }
         }
         fs.close();
 
-        System.out.println("!!!!!!!!!!!!! ending");
-
       }
     }
 
-    System.out.println("!!!!!!!!!!!!! end setLocation");
     super.setLocation(location, job);
   }
 
