ff1817bd1ee0efd2ba7c6561f4d8efa7a4946eaf
Peter <plasne@iname.com>
2017-06-07 15:45:19 -0400

    removed delim

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -1,12 +1,10 @@
 package input;
 
+import java.io.IOException;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.pig.piggybank.storage.CSVLoader;
 
 class LoadCsvOrEmpty extends CSVLoader {
-
-  public LoadCsvOrEmpty(String delim) {
-    super(delim);
-  }
 
   @Override
   public void setLocation(String location, Job job) throws IOException {
