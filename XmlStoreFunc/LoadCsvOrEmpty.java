d22fc973b8e6011b82fd38d43ad1c18b8c3275fc
Peter <plasne@iname.com>
2017-06-07 16:36:48 -0400

    chg

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -6,6 +6,9 @@ import org.apache.pig.piggybank.storage.CSVLoader;
 
 class LoadCsvOrEmpty extends CSVLoader {
 
+  public LoadCsvOrEmpty(String delim) {
+  }
+
   @Override
   public void setLocation(String location, Job job) throws IOException {
     System.out.println("location: " + location);
