61d14507b528e443ed4af95d5a512fd48cfb4149
Peter <plasne@iname.com>
2017-06-08 12:59:41 -0400

    out

--- XmlStoreFunc/LoadCsvOrEmpty.java
+++ XmlStoreFunc/LoadCsvOrEmpty.java
@@ -6,7 +6,13 @@ package input;
 // support configuration of an empty directory
 
 import java.lang.Integer;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.Iterator;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
@@ -17,18 +23,38 @@ import org.apache.pig.piggybank.storage.CSVLoader;
 import org.apache.pig.impl.util.UDFContext;
 import org.apache.pig.PigException;
 import org.apache.pig.PigWarning;
+import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.backend.executionengine.ExecException;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+class Column {
+  public String name;
+  public String type;
+  public String onWrongType;
+
+  public Column(String name, String type, String onWrongType) {
+    this.name = name;
+    this.type = type;
+    this.onWrongType = onWrongType;
+  }
+
+}
 
 public class LoadCsvOrEmpty extends CSVLoader {
 
   boolean hasFiles = false;
+  boolean hasConfig = false;
   String target;
-  int columns;
+  String config;
+  private ArrayList<Column> columns = new ArrayList<Column>();
 
-  public LoadCsvOrEmpty(String target, String columns) {
+  public LoadCsvOrEmpty(String target, String config) {
     this.target = target;
-    this.columns = Integer.parseInt(columns);
+    this.config = config;
   }
 
   @Override
@@ -38,8 +64,22 @@ public class LoadCsvOrEmpty extends CSVLoader {
       if (t != null) {
 
         // verify number of columns
-        if (t.size() != columns) {
-          throw new ExecException("size " + t.size() + " vs " + columns, 2200, PigException.BUG);
+        if (t.size() != columns.size()) {
+          throw new ExecException("size " + t.size() + " vs " + columns.size(), 2200, PigException.BUG);
+        }
+
+        DataType type = t.getType(i);
+        Object value = t.get(i);
+        Column column = columns[i];
+        switch (column.type.toLowerCase()) {
+          case "bool":
+          case "boolean":
+            if (type != DataType.BOOLEAN) throw new ExecException("expected boolean but saw " + type.toString(), 2201, PigException.BUG);
+            break;
+          case "int":
+          case "integer":
+            if (type != DataType.INTEGER) throw new ExecException("expected integer but saw " + type.toString(), 2201, PigException.BUG);
+            break;
         }
 
       }
@@ -51,10 +91,54 @@ public class LoadCsvOrEmpty extends CSVLoader {
 
   @Override
   public void setLocation(String location, Job job) throws IOException {
-    String combiner = location.endsWith("/") ? "" : "/";
-    String folder = location.replace("file:", "") + combiner + target;
+
+    // load config on backend
+    UDFContext udfc = UDFContext.getUDFContext();
+    if (!hasConfig && !udfc.isFrontend()) {
+      try {
+        String raw;
+        
+        // support local and hadoop
+        if (config.startsWith("./")) {
+
+          // read from the local file system
+          raw = new String(Files.readAllBytes(Paths.get(config)), StandardCharsets.UTF_8);
+
+        } else {
+
+          // read from hadoop
+          Configuration conf = job.getConfiguration();
+          Path path = new Path(config);
+          FileSystem fs = FileSystem.get(path.toUri(), conf);
+          FSDataInputStream inputStream = fs.open(path);
+          java.util.Scanner scanner = new java.util.Scanner(inputStream).useDelimiter("\\A");
+          raw = scanner.hasNext() ? scanner.next() : "";
+          fs.close();
+
+        }
+
+        // parse the JSON (need the root before creating the writer)
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(raw);
+        JSONArray columns = (JSONArray) json.get("columns");
+        if (columns != null) {
+          for (int i = 0; i < columns.size(); i++) {
+            JSONObject column = (JSONObject) columns.get(i);
+            String name = column.get("name").toString();
+            String type = column.get("type").toString();
+            String onWrongType = column.get("onWrongType").toString();
+            columns.add(new Column(name, type, onWrongType));
+          }
+        }
+
+      } catch (Exception ex) {
+        throw new ExecException(ex);
+      }
+    }
 
     // support local and hadoop
+    String combiner = location.endsWith("/") ? "" : "/";
+    String folder = location.replace("file:", "") + combiner + target;
     if (folder.startsWith("./")) {
 
       // read from the local file system
@@ -63,7 +147,7 @@ public class LoadCsvOrEmpty extends CSVLoader {
     } else {
 
       // see if there are files to process
-      Configuration conf = job.getConfiguration(); //udfc.getJobConf();
+      Configuration conf = job.getConfiguration();
       FileSystem fs = FileSystem.get(conf);
       Path path = new Path(folder);
       if (fs.exists(path)) {
