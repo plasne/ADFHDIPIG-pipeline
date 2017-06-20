deaabdf700452b00861648fb014a77442017538c
Peter <plasne@iname.com>
2017-06-20 14:22:16 -0400

    toint

--- Pig/src/LoadCsvOrEmpty.java
+++ Pig/src/LoadCsvOrEmpty.java
@@ -318,7 +318,7 @@ public class LoadCsvOrEmpty extends CSVLoader implements LoadMetadata {
         TableQuery<LogEntity> query = TableQuery.from(logging_tableName, LogEntity.class).where("(PartitionKey eq '" + instanceId + "')");
         for (LogEntity entity : cloudTable.getServiceClient().execute(query)) {
           String[] id = entity.getRowKey().split("-");
-          int consider = Integer.toString(id[0]);
+          int consider = Integer.parseInt(id[0]);
           if (consider > instanceIndex) instanceIndex = consider;
         }
         instanceIndex++;
