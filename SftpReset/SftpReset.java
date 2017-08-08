6a27f940593dfa06083dca7d19e5e6e6dfc7e83e
Peter <plasne@iname.com>
2017-08-08 16:42:41 -0400

    changed to mapreduce

--- SftpReset/SftpReset.java
+++ SftpReset/SftpReset.java
@@ -1,63 +1,131 @@
 package com.plasne;
 
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import com.jcraft.jsch.*;
 import java.time.*;
 import java.time.format.*;
 
 public class SftpReset {
-    public static void main(String args[]) {
-        JSch jsch = new JSch();
-        Session session = null;
-        try {
 
-            // arguments
-            int offset = Integer.parseInt(args[0]);
-            int roundTo = Integer.parseInt(args[1]);
-            String input = args[2];
-            String output = args[3];
-            String host = args[4];
-            String user = args[5];
-            String pass = args[6];
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-System.out.print ("host: " + host + "\n");
-System.out.print ("user: " + user + "\n");
-System.out.print ("pass: " + pass + "\n");
+        // defaults
+        int offset = 0;                             // don't offset time
+        int roundTo = 60;                           // round to next hour
+        String input = "input";                     // input folder
+        String output = "'input-'YYYYMMdd'T'HH";    // output folder
+        String hostname = "";                       // hostname for SFTP server
+        String username = "";                       // username for SFTP server
+        String password = "";                       // password for SFTP server
 
-            // connect via SSH
-            session = jsch.getSession(user, host, 22);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setPassword(pass);
-            session.connect();
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-            // connect via SFTP
-            Channel channel = session.openChannel("sftp");
-            channel.connect();
-            ChannelSftp sftpChannel = (ChannelSftp) channel;
-
-            // offset + round
-            LocalDateTime dt_offset = LocalDateTime.now(Clock.systemUTC()).plusMinutes(offset).withSecond(0).withNano(0);
-            LocalDateTime dt_rounded = dt_offset;
-            if (roundTo != 0) {
-              dt_rounded = dt_rounded.plusMinutes( (60 + roundTo - dt_offset.getMinute()) % roundTo);
+            // get all variables
+            String line = value.toString();
+            String[] keyval line.split("=");
+            switch (keyval[0]) {
+                case "offset":
+                    offset = Integer.parseInt(keyval[1]);
+                    break;
+                case "roundTo":
+                    roundTo = Integer.parseInt(keyval[1]);
+                    break;
+                case "input":
+                    input = keyval[1];
+                    break;
+                case "output":
+                    output = keyval[1];
+                    break;
+                case "hostname":
+                    hostname = keyval[1];
+                    break;
+                case "username":
+                    username = keyval[1];
+                    break;
+                case "password":
+                    input = keyval[1];
+                    break;
             }
-            if (roundTo < 0) {
-              dt_rounded = dt_rounded.plusMinutes( roundTo );
+
+            // apply the changes
+            JSch jsch = new JSch();
+            Session session = null;
+            try {
+
+                // connect via SSH
+                session = jsch.getSession(user, host, 22);
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.setPassword(pass);
+                session.connect();
+
+                // connect via SFTP
+                Channel channel = session.openChannel("sftp");
+                channel.connect();
+                ChannelSftp sftpChannel = (ChannelSftp) channel;
+
+                // offset + round
+                LocalDateTime dt_offset = LocalDateTime.now(Clock.systemUTC()).plusMinutes(offset).withSecond(0).withNano(0);
+                LocalDateTime dt_rounded = dt_offset;
+                if (roundTo != 0) {
+                    dt_rounded = dt_rounded.plusMinutes( (60 + roundTo - dt_offset.getMinute()) % roundTo);
+                }
+                if (roundTo < 0) {
+                    dt_rounded = dt_rounded.plusMinutes( roundTo );
+                }
+
+                // rename folder
+                String output_ts = dt_rounded.format(DateTimeFormatter.ofPattern(output));
+                sftpChannel.rename(input, output_ts);
+
+                // create new folder
+                sftpChannel.mkdir(input);
+
+                // logout
+                sftpChannel.exit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (session != null) session.disconnect();
             }
 
-            // rename folder
-            String output_ts = dt_rounded.format(DateTimeFormatter.ofPattern(output));
-            sftpChannel.rename(input, output_ts);
-
-            // create new folder
-            sftpChannel.mkdir(input);
-
-            // logout
-            sftpChannel.exit();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-           if (session != null) session.disconnect();
         }
+
     }
+
+    public static class Reduce extends Reducer <Text, IntWritable, Text, IntWritable> {
+
+        public void reduce(Text key, Iterator<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // nothing to do
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf);
+        job.setJarByClass(WordCount.class);
+        job.setJobName("sftpreset");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapperClass(WordCount.Map.class);
+        job.setCombinerClass(WordCount.Reduce.class);
+        job.setReducerClass(WordCount.Reduce.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.waitForCompletion(true);
+    }
+
 }
