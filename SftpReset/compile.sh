3b445f42d69a4aeb41d77fcb966ffc44d7af55b1
Ubuntu <sshuser@hn0-pelasn.uijoz4u2k3qubijgrtfxoegbig.bx.internal.cloudapp.net>
2017-08-09 14:52:03 +0000

    compiled

--- SftpReset/compile.sh
+++ SftpReset/compile.sh
@@ -1,4 +1,3 @@
-javac -classpath "/usr/hdp/2.5.5.3-2/hadoop/hadoop-common-2.7.3.2.5.5.3-2.jar:/usr/hdp/2.5.5.3-2/hadoop-mapreduce/hadoop-mapreduce-client-core-2.7.3.2.5.5.3-2.jar:/usr/hdp/2.5.5.3-2/pig/lib/json-simple-1.1.jar:jsch.jar" SftpReset.java
-mv SftpReset.class com/plasne/SftpReset.class
+javac -classpath "/usr/hdp/2.5.5.3-2/hadoop/hadoop-common-2.7.3.2.5.5.3-2.jar:/usr/hdp/2.5.5.3-2/hadoop-mapreduce/hadoop-mapreduce-client-core-2.7.3.2.5.5.3-2.jar:SftpReset.jar" SftpReset.java
+mv *.class com/plasne/
 jar vcfm SftpReset.jar Manifest.txt com
-
